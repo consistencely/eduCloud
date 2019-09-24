383e7f07b3a5b1e6372c3ae1696a4633edaf8f7d
linps <linps@example.com>
2019-09-24 17:05:05 +0800

    项目初始化

---
+++ educloud-gateway/src/main/java/com/xuegao/EducloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.xuegao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EducloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducloudGatewayApplication.class, args);
+	}
+
+}
